fc8d6891af9e70844146710dab28f78c4b8e770d
Matteo Manuelli <matteo.manuelli001@gmail.com>
2023-04-04 22:41:38 +0200

    added easymotion keybindings

---
+++ nvim/after/plugin/vim-easymotion.lua
@@ -0,0 +1,4 @@
+vim.keymap.set({"n", "o", "v"}, "f", "<Plug>(easymotion-fl)", { noremap = true })
+vim.keymap.set({"n", "o", "v"}, "F", "<Plug>(easymotion-Fl)", { noremap = true })
+vim.keymap.set({"n", "o", "v"}, "t", "<Plug>(easymotion-tl)", { noremap = true })
+vim.keymap.set({"n", "o", "v"}, "T", "<Plug>(easymotion-Tl)", { noremap = true })
